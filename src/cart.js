aff1d23366295135206a603b2af909e2e40e60f2
htetsoemoe <soemoehtet.mdy@gmail.com>
2022-11-27 17:06:28 +0630

    finished design, layout for home and cart page

--- src/cart.js
+++ src/cart.js
@@ -27,24 +27,22 @@ let generateCartItems = () => {
                 
                     <div class="details">
                     
-                    <div class="title-price-x">
-                        <div class="title-price">
-                        <p>${name}</p>
-                        <p class="cart-item-price">$ ${price}</p>
+                        <div class="title-price-x">
+                            <div class="title-price">
+                                <h3>${name}</h3>
+                                <h4 class="cart-item-price">$ ${price}</h4>
+                            </div>
+                            <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
                         </div>
-                        <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
-                    </div>
-                
-                    <div class="cart-buttons">
-                        <div class="buttons">
-                        <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                        <div id=${id} class="quantity">${item}</div>
-                        <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-                        </div>
-                    </div>
-                
-                    <h3>$ ${item * price}</h3>
                     
+                        <div class="cart-buttons">
+                            <div class="buttons">
+                                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                                <div id=${id} class="quantity">${item}</div>
+                                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                            </div>
+                            <h3>Total: ${item * price}</h3>
+                        </div> 
                     </div>
               </div>
                 `;
@@ -62,3 +60,33 @@ let generateCartItems = () => {
 };
 
 generateCartItems();
+
+
+
+/**
+ * ! Used to calculate total amount of the selected Products
+ * ! with specific quantity
+ * ? When basket is blank, it will show nothing
+ */
+
+ let totalAmount = () => {
+    if (basket.length !== 0) {
+      let amount = basket
+        .map((x) => {
+          let { id, item } = x;
+          let filterData = shopItemsData.find((x) => x.id === id);
+          return filterData.price * item;
+        })
+        .reduce((x, y) => x + y, 0);
+  
+      return (label.innerHTML = `
+      <h2 class="total-amount">Total Bill : $ ${amount}</h2>
+      <div class="btn-group">
+      <button onclick="clearCart()" class="removeAll btn">Clear Cart</button>
+        <button class="checkout btn">Checkout</button>
+      </div>
+      `);
+    } else return;
+  };
+  
+  totalAmount();
